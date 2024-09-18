eb79bbfaf06e88a31f6e9be3161210582a775e69
yaching8896 <118966814+yaching8896@users.noreply.github.com>
2024-09-18 14:34:24 +0800

    Time: 29 ms (93.4%), Space: 16.4 MB (90.86%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        def isMirror(n1, n2):
+            if n1 is None and n2 is None:
+                return True
+            if n1 is None or n2 is None:
+                return False
+            return n1.val == n2.val and isMirror(n1.left, n2.right) and isMirror(n1.right, n2.left)
+        
+        return isMirror(root.left, root.right)
