12bac61c8e77973e24162a46e565cc68b78ef599
yaching8896 <118966814+yaching8896@users.noreply.github.com>
2024-09-19 21:58:43 +0800

    Time: 0 ms (100%), Space: 11.2 MB (88.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        for(int i=0; i < nums2.size(); i++){
+        int i;
+        for(i=0; i<n; i++){
             nums1[m+i] = nums2[i];
         }
-        sort(nums1.begin(), nums1.end());
+
+        std::sort(nums1.begin(), nums1.end());
     }
 };
