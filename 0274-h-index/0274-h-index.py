06c526620304d49910248347a94d3d2dd8161654
yaching8896 <118966814+yaching8896@users.noreply.github.com>
2025-04-02 10:06:32 +0800

    Time: 0 ms (100%), Space: 18.3 MB (21.11%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+
+        for i,v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
+        
